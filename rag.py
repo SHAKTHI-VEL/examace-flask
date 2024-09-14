3893e0cabb5338d15c72a59927dc9e99527f8cb7
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-09-14 16:50:41 +0530

    added cors and preloaded the vector database

--- rag.py
+++ rag.py
@@ -15,24 +15,27 @@ from langchain_mongodb import MongoDBAtlasVectorSearch
 from pymongo import MongoClient
 from langchain_huggingface import HuggingFaceEndpoint
 from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
 import os
 
 load_dotenv()
 
+embeddings = HuggingFaceEmbeddings(model_name="mixedbread-ai/mxbai-embed-large-v1",encode_kwargs = {'precision': 'binary'})
+
+# Vector Store
+one_bit_vectorstore = FAISS.load_local("CN-dataStore", embeddings, allow_dangerous_deserialization=True)
+retriever = one_bit_vectorstore.as_retriever()
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/", methods = ['POST'])
 def hello_world():
         content = request.json
 
         # embeddings
-        embeddings = HuggingFaceEmbeddings(model_name="mixedbread-ai/mxbai-embed-large-v1",encode_kwargs = {'precision': 'binary'})
-
-        # Vector Store
-        one_bit_vectorstore = FAISS.load_local("CN-dataStore", embeddings, allow_dangerous_deserialization=True)
-        retriever = one_bit_vectorstore.as_retriever()
-
         HF_TOKEN = os.getenv("HF_TOKEN")
 
         #llm
@@ -41,9 +44,7 @@ def hello_world():
         #template
         QUERY_PROMPT = PromptTemplate(
         input_variables=["question"],
-        template="""You are an AI  model teaching assistant. Your task is to generate  best versions of the given user question to retrieve relevant documents from
-        a vector database. By generating multiple perspectives on the user question, your
-        goal is to answer the question correctly and help the user overcome some of the limitations of the distance-based similarity search. Provide these alternative questions separated by newlines""",)
+        template="""You are an AI  model teaching assistant. Your task is to generate  best versions of the given user question to retrieve relevant documents from a vector database. By generating multiple perspectives on the user question, your goal is to answer the question correctly and help the user overcome some of the limitations of the distance-based similarity search. Provide these alternative questions separated by newlines""",)
 
         template = """Answer the question based ONLY on the following context:
         {context}
